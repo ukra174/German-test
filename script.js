728ada4a7a43ec42ffdf45cdd194f3497cc390c2
UwU <vania.kazakov2006@gmail.com>
2023-02-24 14:57:25 +0100

    Test?

--- script.js
+++ script.js
@@ -122,3 +122,38 @@ function mobileWrite(){
       renderLine(lines[5],id);
     checkLine();
 }
+
+
+
+
+
+var input = document.getElementById('credit-card-mask'),
+    oldValue,
+    newValue,
+    difference = function(value1, value2) {
+      var output = [];
+      for(i = 0; i < value2.length; i++) {
+        if(value1[i] !== value2[i]) {
+          output.push(value2[i]);
+        }
+      }
+      return output.join("");
+    },
+    keyDownHandler = function(e) {
+      oldValue = input.value;
+      //document.getElementById("onkeydown-result").innerHTML = input.value;
+    },
+    inputHandler = function(e) {
+      newValue = input.value;
+      //document.getElementById("oninput-result").innerHTML = input.value;
+      //document.getElementById("typedvalue-result").innerHTML = difference(oldValue, newValue);
+      
+      userInput[userInput.length-1]+=difference(oldValue, newValue);
+      renderLine(lines[5],id);
+    checkLine();
+
+    }
+;
+
+input.addEventListener('keydown', keyDownHandler);
+input.addEventListener('input', inputHandler);
