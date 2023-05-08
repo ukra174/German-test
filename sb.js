a1fead571a666f5279d36dc947289f7df33f9514
Ivan <77243875+ukra174@users.noreply.github.com>
2023-05-08 19:59:53 +0200

    Added sntenceBuildingPage

---
+++ sb.js
@@ -0,0 +1,36 @@
+
+const ignoreWords = ["ist","sind","bin","seid","bist","werden","werde","wird","werdet","werdest","wurde","wurden","wurdet","wurdest","als","wie","dass","auch","nur","wegen","von","aus","auf","mit","ohne"];
+async function loadText(url) {
+        try {
+        const response = await fetch(url);
+        const data = await response.text();
+        text = data;
+        var sentences = text.split(".");
+        var text = sentence[Math.floor(Math.random()*sentences.length)];
+        var words = text.split(" ");
+        var wordCount = Math.floor(words.length/2);
+        var start = 1+Math.round(Math.random()*(words.length-wordCount));
+        var newText = "";
+        var prompt = "(";
+        for(let i = 0;i<words.length;i++){
+            if(i<start || i>(start+wordCount)){
+                newText+=words[i]+"\n";
+            }else{
+                if(!word[i].replace(",","").replace(".","").lower() in ignoreWords){
+                    prompt+=word[i]+", ";
+                    newText+="_______";
+                }
+            }
+        }
+        prompt+=")";
+        document.getElementById("sentence").text = newText;
+        document.getElementById("spoiler").text = prompt;
+
+        } catch (err) {
+        console.error(err);
+        }
+    }
+    function newSentence(){
+
+  }
+  document.addEventListener("load",loadText("text.txt"));
