dd2e365df47026594e5844147f041b02738d75ba
Ivan <77243875+ukra174@users.noreply.github.com>
2023-05-08 20:11:04 +0200

    Added "Regenerate" button

--- sb.js
+++ sb.js
@@ -1,12 +1,20 @@
 
 const ignoreWords = ["ist","sind","bin","seid","bist","werden","werde","wird","werdet","werdest","wurde","wurden","wurdet","wurdest","als","wie","dass","auch","nur","wegen","von","aus","auf","mit","ohne"];
+var allText = "";
+var sentences = [];
 async function loadText(url) {
         try {
         const response = await fetch(url);
         const data = await response.text();
-        var allText = data;
-        var sentences = allText.split(".");
-        var text = sentence[Math.floor(Math.random()*sentences.length)];
+        allText = data;
+        sentences = allText.split(".");
+        newSentence();
+        } catch (err) {
+        console.error(err);
+        }
+    }
+    function newSentence(){
+        var text = sentences[Math.floor(Math.random()*sentences.length)];
         var words = text.split(" ");
         var wordCount = Math.floor(words.length/2);
         var start = 1+Math.round(Math.random()*(words.length-wordCount));
@@ -25,12 +33,5 @@ async function loadText(url) {
         prompt+=")";
         document.getElementById("sentence").text = newText;
         document.getElementById("spoiler").text = prompt;
-
-        } catch (err) {
-        console.error(err);
-        }
-    }
-    function newSentence(){
-
   }
   document.addEventListener("load",loadText("text.txt"));
