77fc903bc89022f2ee6c9b3205cd68e19c23228d
ukra174 <vania.kazakov2006@gmail.com>
2023-05-23 01:57:14 +0200

    Fixes some situations where word gap had !;., etc.

--- script.js
+++ script.js
@@ -86,6 +86,10 @@ async function loadText(url) {
     }
     for(var i = 0;i<linesReady[id].length;i++){
       if(linesReady[id][i]=="_"){
+        if(!text[id][i].match(/[a-zA-ZäöüßÄÖÜẞ]/i)){
+          outText+=text[id][i];
+          continue;
+        }
         if(userInput[id]!= undefined && userInput[id][index]!=undefined){
           if(userInput[id][index]==text[id][i]){
               outText+="<span class='right'>"+userInput[id][index]+"</span>";
